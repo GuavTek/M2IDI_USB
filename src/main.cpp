01bce9dbd05fb37236dab4b1d14c6efd3bb55aa5
GuavTek <36814224+GuavTek@users.noreply.github.com>
2024-01-20 19:52:35 +0100

    Add CAN driver

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,13 @@
 #include <stdio.h>
 #include "pico/stdlib.h"
+#include <hardware/spi.h>
+#include <hardware/irq.h>
+#include <hardware/timer.h>
 #include "usb_descriptors.h"
 #include "MIDI_Config.h"
 #include "SPI_RP2040.h"
-#include "MCP2517.cpp"
-#include "MIDI_Driver.cpp"
+#include "MCP2517.h"
+#include "MIDI_Driver.h"
 //#include "DMA_driver.h"
 //#include "I2S_driver.h"
 //#include "Audio_driver.h"
@@ -12,10 +15,11 @@
 #include "RingBuffer.h"
 #include "usb.h"
 
-SPI_RP2040_C SPI = SPI_RP2040_C();
-MCP2517_C CAN = MCP2517_C(&SPI);
+SPI_RP2040_C SPI_CAN = SPI_RP2040_C(spi1);
+MCP2517_C CAN = MCP2517_C(&SPI_CAN);
 RingBuffer<8, MIDI_UMP_t> canBuffer;
 
+void dma1_irq_handler ();
 void CAN_Receive_Header(CAN_Rx_msg_t* data);
 void CAN_Receive_Data(char* data, uint8_t length);
 void audio_task(void);
@@ -30,13 +34,18 @@ MIDI_C MIDI_USB(1);
 MIDI_C MIDI_CAN(2);
 
 uint32_t midiID = 239;
+MIDI_UMP_t lostMsg;
+bool hasLost;
 
 int main(void){
 	gpio_init(LEDH);
 	gpio_init(LEDD);
 	gpio_set_dir(LEDH, GPIO_OUT);
 	gpio_set_dir(LEDD, GPIO_OUT);
-	//SPI.Init(SPI_CONF);
+	gpio_init(M2IDI_CAN_INT_PIN);
+	gpio_set_dir(M2IDI_CAN_INT_PIN, GPIO_IN);
+
+	SPI_CAN.Init(SPI_CAN_CONF);
 	//i2s_init(44100);
 	
 	//dma_init(base_descriptor, wrback_descriptor);
@@ -51,11 +60,11 @@ int main(void){
 	MIDI_USB.Set_handler(MIDI_USB_UMP_handler);
 	
 	// Enable SPI interrupt
-	// TODO
+	irq_set_exclusive_handler(DMA_IRQ_1, dma1_irq_handler);
 
-	//CAN.Init(CAN_CONF);
-	//CAN.Set_Rx_Header_Callback(CAN_Receive_Header);
-	//CAN.Set_Rx_Data_Callback(CAN_Receive_Data);
+	CAN.Init(CAN_CONF);
+	CAN.Set_Rx_Header_Callback(CAN_Receive_Header);
+	CAN.Set_Rx_Data_Callback(CAN_Receive_Data);
 	
     while (true){
 		// USB tasks
@@ -72,22 +81,97 @@ int main(void){
 		//	gpio_set(LEDD, 0);
 		//}
 		
-		//if (CAN.Ready()){
-		//	check_can_int();
-		//}
-		//if (canBuffer.Count() && CAN.Ready()){
-		//	char tempData[16];
-		//	MIDI_UMP_t msg;
-		//	uint8_t length;
-		//	
-		//	canBuffer.Read(&msg);
-		//	length = MIDI_CAN.Encode(tempData, &msg, 2);
-		//	CAN_Tx_msg_t txMsg;
-		//	txMsg.dataLengthCode = CAN.Get_DLC(length);
-		//	txMsg.payload = tempData;
-		//	txMsg.id = midiID;
-		//	//CAN.Transmit_Message(&txMsg, 2);
-		//}
+		if (CAN.Ready()){
+			check_can_int();
+		}
+		if (canBuffer.Count() && CAN.Ready()){
+			static int8_t numBytes = 0;
+			char buff[16];
+			MIDI_UMP_t msg;
+			uint8_t length;
+			
+			if (hasLost) {
+				// Recover lost message 
+				hasLost = false;
+				msg = lostMsg;
+			} else {
+				canBuffer.Read(&msg);
+			}
+			length = MIDI_CAN.Encode(buff, &msg, 2);
+
+			// TODO: this will probably break with multiple devices
+			if (msg.type == MIDI_MT_E::Data128) {
+				if (numBytes == 0){
+					// Send first UMP
+					CAN_Tx_msg_t outMsg;
+					outMsg.dataLengthCode = CAN.Get_DLC(length);
+					outMsg.id = (midiID & 0x7F)|(int(MIDI_MT_E::Data128) << 7);
+					outMsg.payload = buff;
+					outMsg.bitrateSwitch = true;
+					CAN.Write_Message(&outMsg, 2);
+				} else {
+					CAN.Append_Payload(buff, length);
+				}
+				numBytes += length;
+				if (msg.data128.status == MIDI2_DATA128_E::Single){
+					CAN.Send_Message();
+					numBytes = 0;
+				} else if (msg.data128.status == MIDI2_DATA128_E::End){
+					CAN.Send_Message();
+					numBytes = 0;
+				} else if (numBytes > 56){
+					CAN.Send_Message();
+					numBytes = 0;
+				}
+			} else if (msg.type == MIDI_MT_E::Data64){
+				if (numBytes == 0){
+					// Send first UMP
+					CAN_Tx_msg_t outMsg;
+					outMsg.dataLengthCode = CAN.Get_DLC(length);
+					outMsg.id = (midiID & 0x7F)|(int(MIDI_MT_E::Data64) << 7);
+					outMsg.payload = buff;
+					outMsg.bitrateSwitch = true;
+					CAN.Write_Message(&outMsg, 2);
+				} else {
+					CAN.Append_Payload(buff, length);
+				}
+				numBytes += length;
+				if (msg.data64.status == MIDI2_DATA64_E::Single){
+					CAN.Send_Message();
+					numBytes = 0;
+				} else if (msg.data64.status == MIDI2_DATA64_E::End){
+					CAN.Send_Message();
+					numBytes = 0;
+				} else if (numBytes > 56){
+					CAN.Send_Message();
+					numBytes = 0;
+				}
+			} else if (msg.type == MIDI_MT_E::RealTime){
+				if (numBytes != 0){
+					// In the middle of sysex
+					CAN.Send_Message();
+					numBytes = 0;
+					lostMsg = msg;
+					hasLost = true;
+				} else {
+					// Send CAN message
+					CAN_Tx_msg_t outMsg;
+					outMsg.dataLengthCode = CAN.Get_DLC(length);
+					outMsg.id = (midiID & 0x7F)|(int(MIDI_MT_E::RealTime) << 7);
+					outMsg.payload = buff;
+					CAN.Write_Message(&outMsg, 2);
+					CAN.Send_Message();
+				}
+			} else {
+				// Send CAN message
+				CAN_Tx_msg_t outMsg;
+				outMsg.dataLengthCode = CAN.Get_DLC(length);
+				outMsg.id = (midiID & 0x7F)|(int(msg.type) << 7);
+				outMsg.payload = buff;
+				CAN.Write_Message(&outMsg, 2);
+				CAN.Send_Message();
+			}
+		}
 		
 		static uint32_t timrr = 0;
 		if (timrr + blinkTime < time_us_32())	{
@@ -103,20 +187,20 @@ int main(void){
 void CAN_Receive_Header(CAN_Rx_msg_t* data){
 	// Detect CAN id, and MIDI muid collisions
 	// TODO
-	//MIDI_CAN.Decode(data->payload, CAN.Get_Data_Length(data->dataLengthCode) );
+
 }
 
 // Handle MIDI CAN data
 void CAN_Receive_Data(char* data, uint8_t length){
 	// Receive MIDI payload from CAN
-	//MIDI_CAN.Decode(data->payload, CAN.Get_Data_Length(data->dataLengthCode) );
+	MIDI_CAN.Decode(data, length);
 }
 
 void check_can_int(){
 	// Check interrupt pin to start reading from CAN controller
-	//if (!port_pin_get_input_level(PIN_PA00)){
-	//	CAN.Check_Rx();
-	//}
+	if (!gpio_get(M2IDI_CAN_INT_PIN)){
+		CAN.Check_Rx();
+	}
 }
 
 void MIDI_CAN_UMP_handler(struct MIDI_UMP_t* msg){
@@ -146,4 +230,9 @@ void midi_task(void){
 	//	length = tud_midi_stream_read(packet, 16);
 	//	MIDI_USB.Decode((char*)(packet), length);
 	//}
+
+}
+
+void dma1_irq_handler (){
+	SPI_CAN.Handler();
 }
